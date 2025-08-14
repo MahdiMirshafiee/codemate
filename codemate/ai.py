1171b8b33ea5d804b7d7872fc63150fc2cb58539
MahdiMirshafiee <Mirshafieemahdi001@gmail.com>
2025-08-14 18:19:46 +0330

    refactor: seprate ai api fun

---
+++ codemate/ai.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+import sys
+from codemate.api_manager import get_api_key
+
+def call_gpt(content, mode='debug'):
+    apikey = get_api_key()
+    if not apikey:
+        print("API Key not set. Please run 'codemate config'")
+        sys.exit(1)
+    client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key = apikey
+    )
+
+    completion = client.chat.completions.create(
+    model="openai/GPT-4o",
+    messages=[
+{
+        "role": "system",
+        "content": """
+        """},        
+{
+        "role": "user",
+        "content": """
+        """
+}
+    ],
+    temperature=0,
+    max_tokens=1500,
+)
+
+    return completion.choices[0].message.content
